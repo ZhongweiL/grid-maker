1f5d45ddd6f5db98e7d35b69f13618eb291187c5
Zhongwei Li <li@Zhongweis-Air.lan>
2022-03-18 22:45:21 -0400

    delete alert

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ let colorSelected;
 
 // Add a row
 function addR() {
-    alert("Clicked Add Row"); // Replace this line with your code.
     let table = document.getElementById("grid").children[0]; // select the table
     let newRow = table.insertRow(); // create a new roll
     for (let i = 0; i < numCols; i++) newRow.insertCell(); // add columns for the new row
